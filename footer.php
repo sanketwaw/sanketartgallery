cd86bbb35e76c8f720908b83998a9fa6e2dbc663
Sanket, Wawale <sanket.wawale@gmail.com>
2022-01-30 11:56:17 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="contactus.php">Contact Us</a></li>
+            </ul> 
+                 <p>@copyright's
+             </div>
+            
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include('header.php'); ?>
+        <div id="container">
+            <div id="content">
+                  <div id="contentleft">
+                      <h3>MY ART WORKS</h3>
+                      <div id="outerbox">
+            <div id="innerbox">
+              <img src="images/11.jpg">
+              <img src="images/12.jpg">
+             <img src="images/13.jpg">
+              <img src="images/14.jpg">
+              <img src="images/15.jpg">
+              <img src="images/16.jpg">
+              <img src="images/17.jpg">
+              <img src="images/18.jpg">
+              <img src="images/19.jpg">
+              <img src="images/20.jpg">
+
+              
+                </div>
+            
+            </div>
+        
+                  </div>
+                  <?php include ('sidebar.php'); ?>
+
+                  </div>
+            </div>
+        
+    <?php include('footer.php'); ?>
