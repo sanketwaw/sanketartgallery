d05eeff6b9f982599e44806b391e902c7e785fd2
Sanket, Wawale <sanket.wawale@gmail.com>
2022-02-20 09:38:40 +0530

    added php files

---
+++ aboutus.php
@@ -0,0 +1,15 @@
+<?php include('header.php'); ?>
+        <div id="container">
+            <div id="content">
+                  <div id="contentleft">
+                      <h3>Want to BUY ?</h3>
+                      <h4>If You want to buy any of the art work please contact to this 
+                      number:- 7387354806</h4> </p>
+
+           </div>
+                  <?php include ('sidebar.php'); ?>
+
+                  </div>
+            </div>
+        
+    <?php include('footer.php'); ?>
---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Meadow Solar</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    <body>
+        <div id="header">
+            <img src="images/logo.png">   
+        </div>
+       
+        <div id="navigation">
+            <ul>
+                <li><a href="index.php">MY ART WORKS</a></li>
+                <li><a href="aboutus.php">WANT TO BUY ?</a></li>
+                <li><a href="contactus.php">CONTACT</a></li>
+            </ul>  
+        </div>
+        
