c00deaf0638624c267cfb1fd1c53201a3dfc95a8
Sanket, Wawale <sanket.wawale@gmail.com>
2022-02-25 22:15:03 +0530

    creating project again

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 #creating an aws instance
 resource "aws_instance" "webservers" {
-  count = 2
   ami             = "ami-0341aeea105412b57"
   instance_type   = "t2.micro"
   subnet_id       = "subnet-8fb4d0f7"
   key_name        = "Windows"
-  security_groups = [sg-0f30ecdf26ae644db]
+  count = 2
+  security_groups = ["sg-0f30ecdf26ae644db"]
   
 tags = {
     Name = "computer"
